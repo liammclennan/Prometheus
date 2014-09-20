bd7cddfac0b1972ee581aab00a6f91342dced68c
Liam McLennan <liam.mclennan@gmail.com>
2014-09-20 12:56:43 +1000

    improved error handling

--- RecordsToTypescript/Fire.fs
+++ RecordsToTypescript/Fire.fs
@@ -15,11 +15,14 @@ let private fieldToCtorDef (f:PropertyInfo) : string =
 let private convert t : string =
     let fields = FSharpType.GetRecordFields t
     let ctorDef = System.String.Join(", ", fields |> Array.map fieldToCtorDef)
-    let wrapNs cls = if t.Namespace <> null && t.Namespace <> "" then (t.Namespace, cls) ||> sprintf "module %s { %s\n}"  else cls
+    let wrapNs cls = if t.Namespace <> null && t.Namespace <> "" then 
+                        (t.Namespace, cls) ||> sprintf "module %s { %s\n}\n"  
+                     else 
+                        cls
     sprintf """
-export class %s {
-    constructor(%s) {}
-}""" t.Name ctorDef
+    export class %s {
+        constructor(%s) {}
+    }""" t.Name ctorDef
         |> wrapNs
 
 let convertAssembly (a:Assembly) =
--- RecordsToTypescript/Program.fs
+++ RecordsToTypescript/Program.fs
@@ -2,15 +2,32 @@
 open System.IO
 open Fire
 
+let getResult fn =
+    try
+        fn
+            |> System.IO.Path.GetFullPath 
+            |> Assembly.LoadFile 
+            |> convertAssembly 
+            |> Choice1Of2 
+    with 
+        | ex -> 
+            (fn, ex.Message) 
+                ||> sprintf "Failed to load %s. Message: %s" 
+                |> Choice2Of2
+
 [<EntryPoint>]
 let main argv =
     if Array.length argv <> 1 then 
         printfn "Usage: Prometheus <assemblyname>"
         1
     else
-        argv.[0] 
-            |> System.IO.Path.GetFullPath 
-            |> Assembly.LoadFile 
-            |> convertAssembly 
-            |> printfn "%s"
-        0 // return an integer exit code
+        match getResult argv.[0] with
+            | Choice1Of2 v -> 
+                let filename = Path.GetFileNameWithoutExtension(argv.[0]) 
+                                |> sprintf "%s_types.ts"
+                File.WriteAllText(filename, v)
+                (Path.GetFileName(argv.[0]), filename) ||> printfn "All record types in assembly %s written to file %s"
+                0 // return an integer exit code
+            | Choice2Of2 e ->
+                printfn "%s" e
+                1
